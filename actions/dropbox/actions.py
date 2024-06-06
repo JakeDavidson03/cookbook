a4d79f36254343469d76581b88e015d8c6fc1300
Perry Mitchell <perry@perrymitchell.net>
2024-06-06 09:33:44 +0200

    Add get file contents

--- actions/dropbox/actions.py
+++ actions/dropbox/actions.py
@@ -9,12 +9,13 @@ https://github.com/sema4ai/actions/blob/master/README.md
 import numbers
 import requests
 import json
+import re
 from sema4ai.actions import action, Secret
 
 DELETE_URL = "https://api.dropboxapi.com/2/files/delete_v2"
 DIRECTORY_CONTENTS_URL = "https://api.dropboxapi.com/2/files/list_folder"
 DIRECTORY_CREATE_URL = "https://api.dropboxapi.com/2/files/create_folder_v2"
-DOWNLOAD_URL = "https://content.dropboxapi.com/2/files/download"
+FILE_CONTENTS_READ_URL = "https://content.dropboxapi.com/2/files/download"
 UPLOAD_URL = "https://content.dropboxapi.com/2/files/upload"
 
 def entry_to_file_item(
@@ -26,6 +27,20 @@ def entry_to_file_item(
         "type": "directory" if entry[".tag"] == "folder" else "file",
         "size": entry["size"] if "size" in entry else 0
     }
+
+def url_safe_range_replace(match) -> str:
+    char = match.group()
+
+    encoded = f"000{hex(ord(char[0]))}"
+
+    return encoded[-4:]
+
+def url_safe_json_dumps(
+    item: dict
+) -> str:
+    output = json.dumps(item, separators=(',', ':'))
+
+    return re.sub(r"[\u007f-\uffff]", url_safe_range_replace, output)
 
 ###########
 # ACTIONS #
@@ -68,11 +83,11 @@ def delete_file(
     remote_path: str
 ) -> str:
     """
-    Delete a remote directory/path on a Dropbox account.
+    Delete a remote directory/file on a Dropbox account.
 
     Args:
         dropbox_token: The access token for an account.
-        remote_path: The remote directory path to delete.
+        remote_path: The remote path to delete.
 
     Returns:
         The deleted path.
@@ -91,6 +106,36 @@ def delete_file(
     response.raise_for_status()
 
     return remote_path
+
+@action
+def get_file_contents(
+    dropbox_token: Secret,
+    remote_path: str
+) -> str:
+    """
+    Get the file contents of a text-based file on a Dropbox account.
+
+    Args:
+        dropbox_token: The access token for an account.
+        remote_path: The remote file path.
+
+    Returns:
+        The text contents of the remote file.
+    """
+
+    response = requests.get(
+        FILE_CONTENTS_READ_URL,
+        headers = {
+            "Authorization": f"Bearer {dropbox_token.value}",
+            "Content-Type": "text/plain",
+            "Dropbox-API-Arg": url_safe_json_dumps({
+                "path": remote_path
+            })
+        }
+    )
+    response.raise_for_status()
+
+    return response.text
 
 @action
 def list_files(
